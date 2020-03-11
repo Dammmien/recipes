15af895f20a0ad14b48bdb45a96e8057733098c2
Damien Duhamel <d.damien@live.fr>
2020-03-11 18:25:38 +0100

    Add recipes

--- src/script.js
+++ src/script.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 fs.readdirSync('./assets/images/recipes/large').forEach(async file => {
   try {
     const img = await Jimp.read(`./assets/images/recipes/large/${file}`);
-    img.resize(400, Jimp.AUTO).quality(60).write(`./assets/images/recipes/thumbnails/${file}`);
+    img.resize(400, Jimp.AUTO).quality(80).write(`./assets/images/recipes/thumbnails/${file}`);
+    console.log( file );
   } catch (e) {
     console.log( e );
   }
